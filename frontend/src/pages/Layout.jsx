abbb0ea33b352379f191a112114716aedbbaf014
DickenMabunda <dickenelegendary@gmail.com>
2025-09-25 14:02:18 +0200

    re-designed the navabr

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,22 +1,31 @@
-import React from 'react'
-import { Outlet, Link } from 'react-router-dom'
+
+import { Link } from 'react-router-dom'
+import { AiFillHome } from 'react-icons/ai';
+import { AiOutlineLogin } from 'react-icons/ai';
+import { FaOpencart } from 'react-icons/fa';
+import { FaBook } from 'react-icons/fa';
 
 function Layout() {
   return (
     <div>
         <nav className='navbar'>
             <div className='navbar-div'>
+                <div className='heading'>
+                    <span className='FaBook'><FaBook /></span> 
+                    <h1 className='heading-title'>Taskify</h1>
+                </div>
             <ul className='navbar-list'>
                 <li>
-                    <Link to='/'>Home</Link>
+                    
+                    <Link className='link' to='/'><span><AiFillHome className='AiFillHome' /></span> Home</Link>
                 </li>
 
                 <li>
-                    <Link to='/login'>Login</Link>
+                    <Link className='link' to='/login'><span><AiOutlineLogin className='AiFillHome' /></span> Login</Link>
                 </li>
 
                 <li>
-                    <Link to='/register'>Register</Link>
+                    <Link className='link'to='/register'><span><FaOpencart className='AiFillHome' /></span> Register</Link>
                 </li>
             </ul>
             </div>
