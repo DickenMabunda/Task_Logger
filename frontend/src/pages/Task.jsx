91d5085143ec3d9d8f8bff0bf9f882939b591c06
@DickenMabunda <dickenlegendary@gmail.com>
2025-10-10 07:59:43 +0200

    working on the list buttons

--- frontend/src/pages/Task.jsx
+++ frontend/src/pages/Task.jsx
@@ -5,20 +5,70 @@ import { FaCheckCircle, FaHourglassHalf, FaClipboardList } from 'react-icons/fa'
 
 const Task = () => {
     const [description, setDescription] = useState('');
-    const [dueDate, setDueDate] = useState('')
-    const [status, setStatus] = useState('')
+    const [dueDate, setDueDate] = useState('');
+    const [status, setStatus] = useState('');
+    const [title, setTitle] = useState('');
+    const [active1, setActive1]= useState('inactive');
+    const [active2, setActive2]= useState('inactive');
+    const [active3, setActive3]= useState('inactive');
+
+    const handleClick1 = (e)=> {
+        console.log(e.target)
+        if (active1 == 'inactive') {
+            setActive1('active')
+            setActive2('inactive')
+            setActive3('inactive')
+        }
+        else {
+            setActive1('inactive')
+        }
+    }
+
+    const handleClick2 = (e)=> {
+        console.log(e.target)
+        if (active2 == 'inactive') {
+            setActive2('active')
+            setActive1('inactive')
+            setActive3('inactive')
+        }
+        else {
+            setActive2('inactive')
+        }
+    }
+
+    const handleClick3 = (e)=> {
+        console.log(e.target)
+        if (active3 == 'inactive') {
+            setActive3("active")
+            setActive1('inactive')
+            setActive2('inactive')
+        }
+        else {
+            setActive3('inactive')
+        }
+    }
+
 
     return (
      <div>
         <h2>Create a new Task</h2>
         <div>
             <div>
-                <div><p>title</p></div>
+                <div
+                    ><p>title</p>
+                    <input
+                    className="input"
+                    type='text'
+                    name='title'
+                    value={title}
+                    onChange={(e)=> setTitle(e.target.value)}
+                    />
+                </div>
                 <div>
                     <ul>
-                        <li onClick={(e)=> setStatus('todo')}>Todo</li>
-                        <li onClick={(e)=> setStatus('in_progress')}>In_progress</li>
-                        <li onClick={(e)=> setStatus('Done')}>Done</li>
+                        <li className={active1} onClick={handleClick1}>Todo</li>
+                        <li className={active2} onClick={handleClick2}>In_progress</li>
+                        <li className={active3} onClick={handleClick3}>Done</li>
                     </ul>
                 </div>
             </div>
@@ -27,6 +77,7 @@ const Task = () => {
                 <div>
                 <p><FaCheckCircle />Description</p>
                 <input
+                className="input"
                 type='text'
                 name='description'
                 value={description}
@@ -37,6 +88,7 @@ const Task = () => {
                 <div>
                     <p>Due Date <FaHourglassHalf /></p>
                     <input
+                    className="input"
                     type='date'
                     name='due_date'
                     value={dueDate}
